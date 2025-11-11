f253019c19d1968b08cfc24452e2f6bdc76b28d0
andks-me <andreeva.ks.an@gmail.com>
2025-11-11 13:35:57 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Quant',
+};
+
+module.exports = nextConfig;
